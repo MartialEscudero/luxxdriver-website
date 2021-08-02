1eac581be18bb14a4ad5b564c69658e62b1c09a9
Escudero Martial <martial.escudero@gmail.com>
2021-08-02 18:00:39 +0200

    fix responsive

--- src/components/Price.vue
+++ src/components/Price.vue
@@ -7,7 +7,7 @@
       <div>
         <img class="shadow-2xl xl:w-100 md:w-96 sm:w-80 mx-auto" src="../assets/img/price.png">
       </div>
-      <div class="hidden xl:block mt-32">
+      <div class="hidden xl:block mt-32 w-5/6">
         <v-carousel hide-delimiters>
           <v-carousel-item
             v-for="(item,i) in items"
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative flex h-screen w-full h-full bg-no-repeat bg-cover bg-right"
+    class="relative flex h-screen w-full h-full bg-no-repeat bg-cover xl:bg-right sm:bg-center"
     style="background-image: url('https://res.cloudinary.com/do5ghqhjj/image/upload/v1627576273/bg_b512cb37bd.jpg')"
   >
     <div class="absolute bottom-5 left-1/2 transform -translate-x-1/2">
